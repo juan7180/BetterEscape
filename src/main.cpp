6abb54d49087dd8d35d6613c2bcb33db45059ee3
juan7180 <jmj.1329.juanfernando@gmail.com>
2026-04-01 19:10:07 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Geode/Geode.hpp>
+
+#include <Geode/modify/PauseLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(BetterEscapePauseLayer, PauseLayer) {
+	void keyDown(cocos2d::enumKeyCodes key, double timestamp) {
+		if (key == cocos2d::enumKeyCodes::KEY_Escape) {
+			this->onResume(nullptr);
+			return;
+		}
+
+		if (key == cocos2d::enumKeyCodes::KEY_L) {
+			this->onQuit(nullptr);
+			return;
+		}
+
+		PauseLayer::keyDown(key, timestamp);
+	}
+
+	void keyBackClicked() {
+		this->onResume(nullptr);
+	}
+};
